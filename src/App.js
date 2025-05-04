dd8158d5b9704331d4451d7f1b06abbb2e3b293c
Nicholas0901 <60474345+nicholas0901-stack@users.noreply.github.com>
2025-05-05 00:15:23 +0800

    Update toast
    replace alerts

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/loginform';
 import Home from './components/home';
 import Dashboard from "./components/dashboard"; 
+import { Toaster } from 'react-hot-toast';  
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-      {/* Login route */}
-      <Route
-        path="/login"
-        element={
-          <div className="login container">
-            <LoginForm />
-          </div>
-        }
-      />
+      <Toaster position="top-center" reverseOrder={false} />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+        {/* Login route */}
+        <Route
+          path="/login"
+          element={
+            <div className="login container">
+              <LoginForm />
+            </div>
+          }
+        />
 
-      {/* Fallback for undefined routes */}
-      <Route
-        path="*"
-        element={
-          <div className="App">
-            <h2>404 - Page Not Found</h2>
-          </div>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
+        {/* Fallback for undefined routes */}
+        <Route
+          path="*"
+          element={
+            <div className="App">
+              <h2>404 - Page Not Found</h2>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
